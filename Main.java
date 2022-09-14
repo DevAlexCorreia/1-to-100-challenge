1a1a28bccdb5806998f0eaa38fe547e6e44604c7
DevAlexCorreia <111699915+DevAlexCorreia@users.noreply.github.com>
2022-09-14 09:30:00 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,15 +6,15 @@ public class Main {
  
       
       Scanner input = new Scanner(System.in);
-     
+      //Sphere calculation
       
-      double R = input.nextDouble();
-      final double pi =3.14159;
-      double VOLUME = ((4.0/3) * pi * (R*R*R));
+      double R = input.nextDouble();//inputing a Radius variable
+      final double pi =3.14159;//Constant of pi with 5 places decimal
+      double VOLUME = ((4.0/3) * pi * (R*R*R));// formula of Volume 4/3 * pi * R³
           
       
-      System.out.printf("VOLUME = %.3f%n", VOLUME);
-      input.close();
+      System.out.printf("VOLUME = %.3f%n", VOLUME);//Printing a value of volume
+      input.close();//closing a input
       
     }
  
