8763072c158acecec189944a0a1f71f2261f405c
DevAlexCorreia <111699915+DevAlexCorreia@users.noreply.github.com>
2022-09-26 09:11:28 -0300

    Create Code25.java

---
+++ Code25.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+public class Main {
+ 
+  public static void main(String[] args) throws IOException {   
+      
+  int number = 1;
+  
+  while(number<=100){
+    number++;
+    if(number%2==0){
+    System.out.printf("%d%n",number);
+    }
+  }   
+     
+  }   
+}
