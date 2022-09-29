7ffe369bd8fca0886fdb967648ea79da510a995f
DevAlexCorreia <111699915+DevAlexCorreia@users.noreply.github.com>
2022-09-29 20:32:23 -0300

    Update Code30.java

--- Code30.java
+++ Code30.java
@@ -9,38 +9,38 @@ public class Main {
     int []a= new int[n];
     System.lineSeparator();
     double tam = a.length;
-    
+    String []var=new String[n];
     for(int i=0;i<=tam;i++){//For de entrada
       a[i]=input.nextInt();
     }
     for(int i=0;i<=tam;i++){//For de Condições
       if(a[i]>0){//Positivo
         if(a[i]%2==0){//par
-          System.out.printf("EVEN POSITIVE");
-        
+            var[i]="EVEN POSITIVE";                   
         }
       }
       if(a[i]<0){//negativo
         if(a[i]%2==0){//par
-          System.out.printf("EVEN NEGATIVE");
-        
+            var[i]="EVEN NEGATIVE"; 
+                  
         }
       }
       if(a[i]>0){//positivo
         if(a[i]%2==1){//impar
-          System.out.printf("ODD POSITIVE");
-        
+            var[i]="ODD POSITIVE";
+                
         }
       }
       if(a[i]<0){//negativo
         if(a[i]%2==1){//impar
-          System.out.printf("ODD NEGATIVE");
-        
+            var[i]="ODD NEGATIVE";
+                 
         }
-      }
-      
+      }    
     }
-   
+    for(int i=0;i<tam;i++){//
+        System.out.printf("%s%n",var[i]);
+    }
     input.close();
   }
 }
