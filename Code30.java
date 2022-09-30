41f125abe2dcd3d6ac1f77290d32df8611d55393
DevAlexCorreia <111699915+DevAlexCorreia@users.noreply.github.com>
2022-09-30 09:51:18 -0300

    Update Code30.java

--- Code30.java
+++ Code30.java
@@ -5,22 +5,22 @@ public class Main {
 
   public static void main(String[] args) throws IOException{
     Scanner input = new Scanner(System.in);
-    int n=input.nextInt();
-    int []a= new int[n];
+    int n = input.nextInt();
+    int[] a = new int[n];
     System.lineSeparator();
-     String []var=new String[n];
+
+    String[] var = new String[n];
     
     for(int i=0;i<a.length;i++){
       a[i]=input.nextInt();
+
       if(a[i]>0){//Positivo
         if(a[i]%2==0){//par
             var[i]="EVEN POSITIVE";                   
-        }
-        else if(a[i]%2==1){
+        }else if(a[i]%2==1){
           var[i]="ODD POSITIVE";              
         }
-      }
-      else if(a[i]<0){
+      }else if(a[i]<0){
         if(a[i]%2==0){
             var[i]="EVEN NEGATIVE";                   
         }
