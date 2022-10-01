aa0a3189c1fe435345d01657ba7806e71b4e8ff3
DevAlexCorreia <111699915+DevAlexCorreia@users.noreply.github.com>
2022-10-01 14:17:22 -0300

    Update Code27.java

--- Code27.java
+++ Code27.java
@@ -5,53 +5,25 @@ public class Main {
   public static void main(String[] args) throws IOException {   
       Scanner input = new Scanner(System.in);
     
-  double n1 = input.nextDouble();
-  System.lineSeparator();
-  double n2 = input.nextDouble();
-  System.lineSeparator();
-  double n3 = input.nextDouble();
-  System.lineSeparator();
-  double n4 = input.nextDouble();
-  System.lineSeparator();
-  double n5 = input.nextDouble();
-  System.lineSeparator();
-  double n6 = input.nextDouble();
-  System.lineSeparator();
+double[] n = new double[6];
   double media = 0,total=0,aux=0;
   int numbers = 0;
    
-      if(n1>0){
-        numbers+=1;
-        aux+=n1;
-        media++;        
-        }if(n2>0){
-        numbers+=1;
-        aux+=n2;
-        media++;        
-      }if(n3>0){
-        numbers++;
-        aux+=n3;
-        media++;       
-      }if(n4>0){
-        numbers++;
-        aux+=n4;
-        media++;
-      }if(n5>0){
-        numbers++;
-        aux+=n5;
-        media++;
-      }if(n6>0){
-        numbers++;
-        aux+=n6;
-        media++;
-      }
-
+  for(int i=0;i<n.length;i++){
+    n[i] = input.nextDouble();
+  }
+  for(int i=0;i<n.length;i++){
+    if(n[i]>0){
+      numbers++;
+      aux++;
+      media++;
+    }
+  }
     total=aux/media;
 
   System.out.printf("%d valores positivos\n",numbers);
   System.out.printf("%.1f%n",total);
  
   input.close();   
-     
   }   
 }
